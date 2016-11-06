47cf7eb3b01223670fe69fd9705013028e8f0fec
Brian Clozel <bclozel@pivotal.io>
2016-11-06 10:48:22 +0100

    Add Ajax and ServerSentEvents endpoints
    This step demonstrates how `Flux`, within a web application, can not
    only convey an "asynchronous list of items" but also a "infinite stream
    of items delivered over SSE".
    
    Note that Spring Web Reactive also provides smarter types to deal with
    SSE in a more fine-grained way: see `ServerSentEvent`.

--- src/main/java/com/example/web/DashboardController.java
+++ src/main/java/com/example/web/DashboardController.java
@@ -4,10 +4,12 @@ import com.example.dashboard.DashboardService;
 import com.example.dashboard.ReactorPerson;
 import com.example.dashboard.ReactorPersonNotFoundException;
 import com.example.dashboard.ReactorPersonRepository;
+import com.example.integration.gitter.GitterMessage;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -64,6 +67,23 @@ public class DashboardController {
 					model.addAttribute("issues", list);
 					return Mono.just("issues");
 				});
+	}
+
+	@GetMapping("/chat")
+	public String chat() {
+		return "chat";
+	}
+
+	@GetMapping(path = "/chatMessages", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public Flux<GitterMessage> chatMessages(@RequestParam(required = false, defaultValue = "10") String limit) {
+		return this.dashboardService.getLatestChatMessages(Integer.parseInt(limit));
+	}
+
+	@GetMapping(value = "/chatStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	@ResponseBody
+	public Flux<GitterMessage> streamChatMessages() {
+		return this.dashboardService.streamChatMessages();
 	}
 
 }
